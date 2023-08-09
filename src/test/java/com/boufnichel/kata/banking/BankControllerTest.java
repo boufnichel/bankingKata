9440767bef837a72267c8d61557b72ec36e1a6da
boufnichel <boufnichel.mohamed@gmail.com>
2023-08-09 23:30:31 +0100

    Init project

---
+++ src/test/java/com/boufnichel/kata/banking/BankControllerTest.java
@@ -0,0 +1,24 @@
+package com.boufnichel.kata.banking;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class BankControllerTest {
+
+    @InjectMocks
+    private BankController bankController;
+
+    @Test
+    void should_return_200_when_amount_is_deposit_positive(){
+        ResponseEntity<String> responseEntity = bankController.deposit(5000d);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
